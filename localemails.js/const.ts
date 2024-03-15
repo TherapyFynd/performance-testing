5500c588c38515c6284adeb368a80b5f1565a9f4
Ayush Kumar Shukla <shuklaayushkumar27@gmail.com>
2024-03-15 17:09:45 +0530

    fix : updated localhost running for e2e test

--- localemails.js/const.ts
+++ localemails.js/const.ts
@@ -1,4 +1,20 @@
 // if true redirect to localhost , when it false it redirect to stage or dev. , prod.
+export const isRunningOnLocal: boolean = false;
+export const localPort: string = '3000';
+export const localBaseUrl = `http:/localhost:${localPort}`;
 
-export const isRunningOnLocal:boolean= true;
-export const localPort:string= "3000";
+const TYPE_OF_URLS_BACKEND = {
+  PROD: 'https://ehr-api.joinleafs.com',
+  STAGE: 'https://leafs-ehr-nest-stage-nmvorvf7ga-as.a.run.app',
+  DEV: 'https://leafs-ehr-nest-dev-nmvorvf7ga-uc.a.run.app',
+};
+
+const TYPE_OF_URLS_FRONTEND = {
+  PROD: 'https://ehr.omnipractice.co',
+  STAGE: 'https://leafs-ehr-web-stage-nmvorvf7ga-as.a.run.app',
+  DEV: 'https://leafs-ehr-web-dev-nmvorvf7ga-uc.a.run.app',
+};
+
+export const BASE_BACKEND_URL = TYPE_OF_URLS_BACKEND.STAGE;
+
+export const BASE_FRONTEND_URL = TYPE_OF_URLS_FRONTEND.STAGE;
