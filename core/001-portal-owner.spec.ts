f7c0714e150badb8171b8d308b33478a3e2768a1
Ayush Kumar Shukla <shuklaayushkumar27@gmail.com>
2024-04-15 14:21:34 +0530

    some logic upd

--- core/001-portal-owner.spec.ts
+++ core/001-portal-owner.spec.ts
@@ -2,6 +2,9 @@ import { test, type Page } from '@playwright/test';
 import { generatePasswordlessLoginLink } from '../helpers/api';
 import { createMailSurpEmail } from '../helpers/mailsurp';
 
+// Annotate entire file as serial.
+test.describe.configure({ mode: 'serial' });
+
 let page: Page;
 
 test.beforeAll(async ({ browser }) => {
