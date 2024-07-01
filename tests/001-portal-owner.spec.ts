c3afbda84122ef3c2f9626f81b6fc6b36c816398
Rajesh Kumar <154408723+Rajesh9380@users.noreply.github.com>
2024-07-01 03:46:34 -0700

    New Feature debug

--- tests/001-portal-owner.spec.ts
+++ tests/001-portal-owner.spec.ts
@@ -92,12 +92,12 @@ test('Owner login and  onboarding ', async ({ request }) => {
   await page.getByRole('button', { name: 'Next' }).nth(1).click();
   await page.getByRole('button', { name: 'Next' }).nth(1).click();
  
-  await page.getByLabel('').check();
-  await page.waitForTimeout(2000);
+  await page.getByLabel('', { exact: true }).check();
+    await page.waitForTimeout(2000);
   await page.getByRole('button', { name: 'Agree & Continue' }).nth(1).click();
   await page.waitForTimeout(2000);
-  await page.getByLabel('').check();
-  await page.getByRole('button', { name: 'Agree & Continue' }).nth(1).click();
+  await page.getByLabel('', { exact: true }).check();
+    await page.getByRole('button', { name: 'Agree & Continue' }).nth(1).click();
 });
 
 test('Settings Flows', async () => {
--- tests/002-portal-therapist.spec.ts
+++ tests/002-portal-therapist.spec.ts
@@ -65,11 +65,11 @@ test('Therapist login and onboarding ', async ({ request }) => {
 
   await page.getByRole('button', { name: 'Next' }).nth(1).click();
 
-  await page.getByLabel('').check();
+  await page.getByLabel('', { exact: true }).check();
   await page.waitForTimeout(2000);
   await page.getByRole('button', { name: 'Agree & Continue' }).nth(1).click();
   await page.waitForTimeout(2000);
-  await page.getByLabel('').check();
+  await page.getByLabel('', { exact: true }).check();
   await page.getByRole('button', { name: 'Agree & Continue' }).nth(1).click();
 });
 
